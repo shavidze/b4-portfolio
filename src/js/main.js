a4ca4cefffd8b7561f1dd5c8aefc96e7e612751f
Saba Shavidze <saba.shavidze@wandio.com>
2019-04-06 17:24:58 +0400

    Added Section: Work

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $(".port-item").click(function() {
     $(".collapse").collapse("hide");
   });
+  $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
 });
